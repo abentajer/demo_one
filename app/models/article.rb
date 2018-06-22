188acbfc007ffaf44372fc898d9341432db73362
bentajer <abentajer@ymagisgroup.com>
2018-06-22 12:42:24 +0100

    fixing bugs in create user

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,10 @@
 class Article < ApplicationRecord
-  validates :title, presence: true, length: {minimum: 2, maximum: 30}
-  validates :description, length: {minimum: 5, maximum: 250}
+  validates :title,
+            presence: true,
+            length: {minimum: 2, maximum: 30}
+
+  validates :description,
+            length: {minimum: 5, maximum: 250}
 
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,20 @@
 class User < ApplicationRecord
   has_many :comments
 
+  before_save {self.email = email.downcase}
+  before_save { self.password = password.hash}
+  before_update {self.password = password.hash}
+
   validates :username,
             presence: true,
             length: {minimum: 2},
             uniqueness: {case_sensitive: false}
 
   VALID_EMAIL_REGEX= /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
   validates :email,
             presence: true,
-            length: {minimum: 2},
+            length: {minimum: 5},
             uniqueness: true,
             format: {with: VALID_EMAIL_REGEX, message: "Not a valid email"}
 
